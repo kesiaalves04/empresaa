ff5f8dadf7254db45c438d3938991535e85cd6fc
kesia alves <kesia.alves@academico.ifpb.edu.br>
2026-05-13 12:08:35 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -19,7 +19,7 @@ col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     st.markdown(f"""
         <div style="text-align: center; margin-bottom: 50px;">
-            <a href="https://www.ifpb.edu.br/" target="_blank">
+            <a href="https://www.apple.com/br/iphone/" target="_blank">
                 <img src="data:image/png;base64,{img_base64}"
                      width="320"
                      style="border-radius:12px;">
